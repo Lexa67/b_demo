2572f33240391fd5c747b151abea4eda6cd5a5e5
Lexa67 <3aj4ik504@gmail.com>
2023-05-16 10:25:03 +0300

    last changes in 2 video

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_06_121022) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_15_202638) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_06_121022) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "views", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
